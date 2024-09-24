26edf4a3609fdd98f5e16764348a504406b2b31c
gautamsarawagi <gautamsarawagi1@gmail.com>
2024-09-24 21:39:29 +0530

    Boilerplate Added
    (cherry picked from commit f1d08498f9d309f89e0693997228931e1c393afc)

---
+++ src/main/java/org/example/evaluations/evaluation/services/GoogleKeyVault.java
@@ -0,0 +1,13 @@
+package org.example.evaluations.evaluation.services;
+
+public class GoogleKeyVault implements IKeyVault{
+
+    @Override
+    public void saveSecret(String secretName, String secretValue) {
+    }
+
+    @Override
+    public String retrieveSecret(String secretName) {
+        return null;
+    }
+}
